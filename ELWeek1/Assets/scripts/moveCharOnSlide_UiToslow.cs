c82ffa2070c3f7d02067a5330d676098a2c0a15a
RebeccaRosies <rebecca.rosies@student.ehb.be>
2022-10-19 09:59:32 +0200

    Feat: second slider
    I added a second slider which doesn't interfere with the first and can also move the character forwards or backwards and is also automatically reset upon letting the handler go so the character slows down gradually

--- ELWeek1/Assets/scripts/moveCharOnSlide_UiToslow.cs
+++ ELWeek1/Assets/scripts/moveCharOnSlide_UiToslow.cs
@@ -12,31 +12,43 @@ public class moveCharOnSlide_UiToslow : MonoBehaviour
 {
 
     CharacterController characterController;
-    [SerializeField] private UnityEngine.UI.Slider slider;
-    [SerializeField] private TextMeshProUGUI slidertext;
+    [SerializeField] private UnityEngine.UI.Slider leftSlider;
+    [SerializeField] private TextMeshProUGUI leftSlidertext;
+    [SerializeField] private UnityEngine.UI.Slider rightSlider;
+    [SerializeField] private TextMeshProUGUI rightSlidertext;
   //---------------------------------------------------------------//
     public GameObject ui_canvas;
+    GameObject ui_element;
     GraphicRaycaster ui_raycaster; // initialize a graphic raycaster -> 17: grab graphicraycaster from ui_canvas 
     PointerEventData click_data;
     List<RaycastResult> click_results;
 
-    float sliderStartValue;
-    float currentSliderValue;
+    float leftSliderStartValue;
+    float currentLeftSliderValue;
+    float currentRightSliderValue;
     float slowDownStart;
-    bool slowDownActive = false;
+    bool slowDownLeftActive = false;
+    bool slowDownRightActive = false;
 
 
     void Start()
     {
-        slider.minValue = -1;
-        slider.maxValue = 1;
+        leftSlider.minValue = -1;
+        leftSlider.maxValue = 1;
+        rightSlider.minValue = -1;
+        rightSlider.maxValue = 1;
 
         characterController = GetComponent<CharacterController>();
 
-        slider.onValueChanged.AddListener((v)=>{
+        leftSlider.onValueChanged.AddListener((v)=>{
            /*  Debug.Log(v); */
-            slidertext.text = v.ToString("0.00"); //value of the string =  #of rotations/min
-            moveCharOnSlide(v);
+            leftSlidertext.text = v.ToString("0.00"); //value of the string =  #of rotations/min
+            moveCharLeftOnSlide(v);
+        });
+        rightSlider.onValueChanged.AddListener((v)=>{
+           /*  Debug.Log(v); */
+            rightSlidertext.text = v.ToString("0.00"); //value of the string =  #of rotations/min
+            moveCharRightOnSlide(v);
         });
         //
         //----------GRAPHICRAYCASTER---------------------------------------------------//
@@ -59,29 +71,56 @@ public class moveCharOnSlide_UiToslow : MonoBehaviour
          if(Mouse.current.leftButton.isPressed){
             GetUIElementsClicked();
         }
-        if(Mouse.current.leftButton.wasReleasedThisFrame){
-            slowDownActive = true;
+
+    // if the left slider was released then make it start to slow down , if the right slider was released then make it start to slow down
+        if(Mouse.current.leftButton.wasReleasedThisFrame && click_results[0].gameObject.transform.parent.parent.name == "SliderLeft"){
+            slowDownLeftActive = true;
+        }
+        if(Mouse.current.leftButton.wasReleasedThisFrame && click_results[0].gameObject.transform.parent.parent.name == "SliderRight"){
+            slowDownRightActive = true;
+        }
+    // if the left or right slider gets back to zero then stop decreasing speed in the respective slider
+        if(currentLeftSliderValue == 0.00f){
+             slowDownLeftActive = false;
+        }
+        if(currentRightSliderValue == 0.00f){
+             slowDownRightActive = false;
         }
 
-        if( currentSliderValue == 0.00f){
-             slowDownActive = false;
+    // if the slider is currently not zero then keep movig it at the # of rotations/minute it is set at
+         if( currentLeftSliderValue != 0.00f){
+             moveCharLeftOnSlide(currentLeftSliderValue);
         }
-         if( currentSliderValue != 0.00f){
-             moveCharOnSlide(currentSliderValue);
+        if( currentRightSliderValue != 0.00f){
+             moveCharRightOnSlide(currentRightSliderValue);
         }
-        if (slowDownActive && currentSliderValue >= 0){
-            resetPositiveSlider();
+    // if the LEFT slider is currently above / below zero and should be slowed down -> decrease speed to 0
+        if (slowDownLeftActive && currentLeftSliderValue >= 0){
+            resetLeftPositiveSlider();
         }
-        if (slowDownActive && currentSliderValue <= 0){
-            resetNegativeSlider();
+        if (slowDownLeftActive && currentLeftSliderValue <= 0){
+            resetLeftNegativeSlider();
+        }
+    // if the RIGHT slider is currently above zero and should be slowed down -> decrease speed to 0
+        if (slowDownRightActive && currentRightSliderValue >= 0){
+            resetRightPositiveSlider();
+        }
+        if (slowDownRightActive && currentRightSliderValue <= 0){
+            resetRightNegativeSlider();
         }
         
     }
-      void moveCharOnSlide(float v){
+      void moveCharLeftOnSlide(float v){
         Vector3 movingVector = new Vector3(0,0,v);
        /*  Debug.Log(movingVector); */
         characterController.Move(new Vector3(0,0,v*2) * Time.deltaTime);
-        currentSliderValue = v;
+        currentLeftSliderValue = v;
+    }
+       void moveCharRightOnSlide(float v){
+        Vector3 movingVector = new Vector3(0,0,v);
+       /*  Debug.Log(movingVector); */
+        characterController.Move(new Vector3(0,0,v*2) * Time.deltaTime);
+        currentRightSliderValue = v;
     }
 
 
@@ -89,10 +128,8 @@ public class moveCharOnSlide_UiToslow : MonoBehaviour
         //** get all UI elements clicked, in order of depth (first detected is first) **//
         //grab mouse position & update click-data
         click_data.position = Mouse.current.position.ReadValue();
-        //clear out previous results 
-        click_results.Clear();
-        //perform Raycast itself -> will return UI elements it comes into contacct with
-        ui_raycaster.Raycast(click_data, click_results);
+        click_results.Clear(); //clear out previous results
+        ui_raycaster.Raycast(click_data, click_results); //perform Raycast itself -> will return UI elements it comes into contact with
 
         foreach(RaycastResult result in click_results){
             GameObject ui_element = result.gameObject;
@@ -105,16 +142,24 @@ public class moveCharOnSlide_UiToslow : MonoBehaviour
         
     }
 
-    void resetPositiveSlider(){
-        /* Debug.Log(currentSliderValue); */
-         if (slider.value != 0){
-        slider.value -= Time.deltaTime;
+    void resetLeftPositiveSlider(){
+         if (leftSlider.value != 0){
+        leftSlider.value -= Time.deltaTime;
          }
     }
-    void resetNegativeSlider(){
-        /* Debug.Log(currentSliderValue); */
-         if (slider.value != 0){
-        slider.value += Time.deltaTime;
+    void resetLeftNegativeSlider(){
+         if (leftSlider.value != 0){
+        leftSlider.value += Time.deltaTime;
+         } 
+    }
+     void resetRightPositiveSlider(){
+        if (rightSlider.value != 0){
+        rightSlider.value -= Time.deltaTime;
          }
+    }
+    void resetRightNegativeSlider(){
+         if (rightSlider.value != 0){
+        rightSlider.value += Time.deltaTime;
+         } 
     }
 }
